d854fb1a4ba132455d487f24fd8c6faea5bab028
putriar <paramitaputria86@gmail.com>
2020-02-07 08:50:51 +0700

    splash, Main menu, Description

---
+++ app/src/main/java/com/stembayo/pariwitara/Description.java
@@ -0,0 +1,14 @@
+package com.stembayo.pariwitara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
